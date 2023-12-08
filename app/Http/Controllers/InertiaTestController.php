f6b72dde71cc1ed267eca122225483cb8dc43de3
likuor <likuor0528@gmail.com>
2023-12-07 17:33:20 -0800

    add: delete

--- app/Http/Controllers/InertiaTestController.php
+++ app/Http/Controllers/InertiaTestController.php
@@ -9,7 +9,9 @@ use App\Models\InertiaTest;
 class InertiaTestController extends Controller
 {
     public function index(){
-        return Inertia::render('Inertia/Index');
+        return Inertia::render('Inertia/Index', [
+            'blogs'=>InertiaTest::all()
+        ]);
     }
 
     public function create(){
@@ -18,7 +20,8 @@ class InertiaTestController extends Controller
 
     public function show($id){
         return Inertia::render('Inertia/Show', [
-            'id' => $id
+            'id' => $id,
+            'blog'=> InertiaTest::findOrFail($id)
         ]);
     }
 
@@ -38,6 +41,16 @@ class InertiaTestController extends Controller
         return to_route('inertia.index')
         ->with([
             'message'=> '登録しました'
+        ]);
+    }
+
+    public function delete($id){
+    $book = InertiaTest::findOrFail($id);
+    $book->delete();
+
+    return to_route('inertia.index')
+        ->with([
+            'message'=> '削除しました'
         ]);
     }
 }
