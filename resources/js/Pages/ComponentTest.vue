e8b04faa12a1d62b5c7106866a3bcaf3c7bf778f
likuor <likuor0528@gmail.com>
2023-12-08 12:29:09 -0800

    add: emit

--- resources/js/Pages/ComponentTest.vue
+++ resources/js/Pages/ComponentTest.vue
@@ -2,26 +2,16 @@
 import GuestLayout from "@/Layouts/GuestLayout.vue";
 import InputLabel from "@/Components/InputLabel.vue";
 import TextInput from "@/Components/TextInput.vue";
-// import { reactive } from "vue";
-// import { Inertia } from "@inertiajs/inertia";
 
-// defineProps({
-//     errors: Object,
-// });
-
-// const form = reactive({
-//     title: null,
-//     content: null,
-// });
-
-// const submitFunction = () => {
-//     Inertia.post("/inertia", form);
-// };
+const emitTest = (e) => console.log(e);
 </script>
 
 <template>
     <GuestLayout>
         <InputLabel value="件名">タイトル</InputLabel>
-        <TextInput modelValue="初期値"></TextInput>
+        <TextInput
+            modelValue="初期値"
+            @update:modelValue="emitTest"
+        ></TextInput>
     </GuestLayout>
 </template>
