9c6b20a54a47517f5c1e4d43e7dee0d3af477d23
likuor <likuor0528@gmail.com>
2023-12-09 16:16:52 -0800

    add:validation and flashmessage

--- app/Http/Requests/StoreItemRequest.php
+++ app/Http/Requests/StoreItemRequest.php
@@ -4,7 +4,7 @@ namespace App\Http\Requests;
 
 use Illuminate\Foundation\Http\FormRequest;
 
-class StoreItemRequest extends FormRequest
+class  StoreItemRequest extends FormRequest
 {
     /**
      * Determine if the user is authorized to make this request.
@@ -13,7 +13,7 @@ class StoreItemRequest extends FormRequest
      */
     public function authorize()
     {
-        return false;
+        return true;
     }
 
     /**
@@ -24,7 +24,9 @@ class StoreItemRequest extends FormRequest
     public function rules()
     {
         return [
-            //
+            'name'=>['required', 'max:50'],
+            'memo'=>['required', 'max:255'],
+            'price'=>['required', 'numeric'],
         ];
     }
 }
--- resources/js/Pages/Items/Index.vue
+++ resources/js/Pages/Items/Index.vue
@@ -1,6 +1,7 @@
 <script setup>
 import AuthenticatedLayout from "@/Layouts/AuthenticatedLayout.vue";
-import { Head } from "@inertiajs/inertia-vue3";
+import { Head, Link } from "@inertiajs/inertia-vue3";
+import FlashMessage from "@/Components/FlashMessage.vue";
 
 defineProps({
     items: Array,
@@ -23,14 +24,16 @@ defineProps({
                     <div class="p-6 text-gray-900">
                         <section class="text-gray-600 body-font">
                             <div class="container px-5 py-8 mx-auto">
+                                <FlashMessage />
                                 <div
                                     class="flex pl-4 my-4 lg:w-2/3 w-full mx-auto"
                                 >
-                                    <button
+                                    <Link
+                                        as="button"
+                                        :href="route('items.create')"
                                         class="flex ml-auto text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded"
-                                    >
-                                        Button
-                                    </button>
+                                        >商品登録
+                                    </Link>
                                 </div>
                                 <div
                                     class="lg:w-2/3 w-full mx-auto overflow-auto"
