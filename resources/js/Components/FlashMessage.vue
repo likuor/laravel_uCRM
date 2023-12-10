b55449e05ef7d0e1c305869ff2cbc8abd277338a
likuor <likuor0528@gmail.com>
2023-12-09 17:16:24 -0800

    add: delete method

--- resources/js/Components/FlashMessage.vue
+++ resources/js/Components/FlashMessage.vue
@@ -7,4 +7,10 @@
     >
         {{ $page.props.flash.message }}
     </div>
+    <div
+        v-if="$page.props.flash.status === 'Danger'"
+        class="bg-red-300 text-white p-4"
+    >
+        {{ $page.props.flash.message }}
+    </div>
 </template>
