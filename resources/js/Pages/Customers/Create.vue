28c40f3bd681ccb10642d67ce13bb824d9716ce0
likuor <likuor0528@gmail.com>
2023-12-17 13:21:31 -0800

    add: customer postcode search

--- resources/js/Pages/Customers/Create.vue
+++ resources/js/Pages/Customers/Create.vue
@@ -4,7 +4,7 @@ import { Head } from "@inertiajs/inertia-vue3";
 import { reactive } from "vue";
 import { Inertia } from "@inertiajs/inertia";
 import BreezeValidationErrors from "@/Components/ValidationErrors.vue";
-
+import { Core as YubinBangoCore } from "yubinbango-core2";
 defineProps({
     errors: Object,
 });
@@ -20,6 +20,12 @@ const form = reactive({
     gender: null,
     memo: null,
 });
+
+const fetchAddress = () => {
+    new YubinBangoCore(String(form.postcode), (value) => {
+        form.address = value.region + value.locality + value.street
+    })
+}
 
 const storeCustomer = () => {
     Inertia.post("/customers", form);
@@ -130,6 +136,7 @@ const storeCustomer = () => {
                                                         type="number"
                                                         id="postcode"
                                                         name="postcode"
+                                                        @change='fetchAddress'
                                                         v-model="form.postcode"
                                                         class="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
                                                     />
